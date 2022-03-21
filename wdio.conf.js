0aa1d289a159eaafd4bb2bc10507e3635baa835e
Murali Ramanujam <rmkrishnann@gmail.com>
2022-03-21 17:09:43 +0000

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -94,7 +94,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://myaccount.ee.co.uk/app/dashboard-paym',
+    baseUrl: 'https://google.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
